d6f4facb9a134dd14774b7c8d45d6029b3519866
thiago <thiagoassissilva619@gmail.com>
2026-02-06 08:53:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def contato():
     return render_template("contato.html", sent=sent)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
